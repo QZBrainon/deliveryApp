028faa65546aa7733b2478506dcce52879307ab6
Carlos Alberto Souza Lima Júnior <lima_bigs@yahoo.com.br>
2022-12-07 17:47:34 -0300

    Backend - Tela de detalhe de pedidos - corrigindo erros de lint novamente

--- back-end/src/routes/userRouter.js
+++ back-end/src/routes/userRouter.js
@@ -4,8 +4,8 @@ const { userController } = require('../controllers');
 const router = express.Router();
 
 router.post('/', userController.createUser);
-router.get('/', userController.getAllUsers)
+router.get('/', userController.getAllUsers);
 router.get('/sellers', userController.getSellers);
-router.delete('/:id', userController.deleteUser)
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;

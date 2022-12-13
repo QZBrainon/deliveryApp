1319f63a48cf981c7810fc654b35377186e248eb
Mylena Nunes <mylenanunesdealmeida112@gmail.com>
2022-12-13 10:46:03 -0300

    Adiciona atualizações na pages Orders

--- front-end/src/components/OrderCard.jsx
+++ front-end/src/components/OrderCard.jsx
@@ -1,34 +1,52 @@
 import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-export default function OrderCard({ id, price, date, status }) {
-  const TIME_MIN = 3;
+export default function OrderCard({ id, status, date, price, role }) {
+  const navigate = useNavigate();
+
+  const rawDate = date.split('T')[0];
+  const [year, month, day] = rawDate.split('-');
+  const formartedDate = `${day}/${month}/${year}`;
+
+  const handleClick = () => {
+    navigate(`/${role}/orders/${id}`);
+  };
 
   return (
     <div
-      style={ {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      } }
+    style={ {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    } }
     >
-      <h5 data-testid={ `customer_orders__element-order-id-${id}` }>
-        {`Pedidos: ${id.toString().padStart(TIME_MIN, '0')}`}
-      </h5>
-      <h2 data-testid={ `customer_orders__element-delivery-status-${id}` }>{status}</h2>
-      <h3
-        data-testid={ `customer_orders__element-order-date-${id}` }
+  <div
+    className="cardOrders"
+    onClick={ handleClick }
+    onKeyDown={ handleClick }
+    role="button"
+    tabIndex={ 0 }
+    >
+    <span>Pedido </span>
+    <span data-testid={ `${role}_orders__element-order-id-${id}` }>{ id }</span>
+    <p data-testid={ `${role}_orders__element-delivery-status-${id}` }>{ status }</p>
+    <p data-testid={ `${role}_orders__element-order-date-${id}` }>{ formartedDate }</p>
+    <p
+      data-testid={ `${role}_orders__element-card-price-${id}` }
       >
-        {date}
-      </h3>
-      <div data-testid={ `customer_orders__element-card-price-${id}` }>{price}</div>
+      { price.replace('.', ',') }
+    </p>
     </div>
-  );
+  </div>
+);
 }
 
 OrderCard.propTypes = {
-  id: PropTypes.number.isRequired,
-  price: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-  status: PropTypes.string.isRequired,
-};
+    id: PropTypes.number.isRequired,
+    status: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    price: PropTypes.string.isRequired,
+    role: PropTypes.string.isRequired,
+  };
--- front-end/src/pages/Orders.jsx
+++ front-end/src/pages/Orders.jsx
@@ -1,45 +1,47 @@
-import React from 'react';
-import Header from '../components/Header';
+import React, { useState, useEffect } from 'react';
+import Header from '../components/Header'
 import OrderCard from '../components/OrderCard';
-import userSales from '../mock/salesMock';
-// import httpRequest from '../axios/config';
 
 function Orders() {
-  // const [orders, setOrders] = useState([]);
+  const [orders, setOrders] = useState([]);
+  // const [token, setToken] = useState([]);
+  const [roleState, setRoleState] = useState('');
 
-  // const fetchOrders = async () => {
-  //   await httpRequest.get('')
-  //     .then(({ data }) => {
-  //       setOrders(data);
-  //     });
-  // };
-
-  // const renderOrders = () => fetchOrders.map((i) => (<OrderCard
-  //   key={ i.id }
-  //   id={ i.id }
-  //   date={ i.date }
-  //   status={ i.status }
-  //   price={ i.price }
-  // />));
-
-  // useEffect(() => {
-  //   fetchOrders();
-  // }, []);
-
+  useEffect(() => {
+    const getOrders = async () => {
+      const user = localStorage.getItem('user');
+      const { id, role, token } = JSON.parse(user);
+      const fetchOrders = await fetch(`http://localhost:3001/sales/user?id=${id}&role=${role}`, {
+        method: 'GET',
+        headers: {
+          authorization: token,
+        },
+      });
+      const ordersJson = await fetchOrders.json();
+      setOrders(ordersJson);
+      setRoleState(role);
+    };
+    getOrders();
+  }, []);
+  
   return (
-    <>
+    <div>
       <Header />
-      <section>
-        {userSales.map((i) => (<OrderCard
-          key={ i.id }
-          id={ i.id }
-          date={ i.saleDate.toLocaleDateString() }
-          status={ i.status }
-          price={ i.total_price }
-        />))}
-      </section>
-    </>
+      <h1>Meus Pedidos</h1>
+      <div className="list">
+        { orders.length > 0
+          ? orders.map((i) => (
+            <OrderCard
+              key={ i.id }
+              id={ i.id }
+              status={ i.status }
+              date={ i.saleDate }
+              price={ i.totalPrice }
+              role={ roleState }
+            />
+          )) : <p>Não há pedidos</p> }
+      </div>
+    </div>
   );
 }
-
 export default Orders;
